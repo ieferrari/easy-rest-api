169e74734276483dfacba4fdaec2f7ae78ea4927
ieferrari <ivanferrarigalizia@gmail.com>
2021-05-26 01:34:30 -0300

    new api fixed secret key and hosts

--- restapi/settings.py
+++ restapi/settings.py
@@ -11,21 +11,29 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+VERBOSE=True # verbose mode for development messages on terminal
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=w0!cv#xnqz1ydp0m4152f4#fmnv3(a7b@z%8poqtd63ok6)8s'
+################################################################################
+try:
+    from .secrets import * # on development SECRET_KEY is  in secrets.py (gitignored)
+except ModuleNotFoundError:
+    SECRET_KEY=os.environ.get('SECRET_KEY') # on deployment SECRET_KEY is an environment variable
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+    REGISTRATION_ADMINS = os.environ.get('REGISTRATION_ADMINS')
+################################################################################
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*','192.168.0.108','127.0.0.1', 'easy-rest-api.herokuapp.com']
 
 
 # Application definition
@@ -72,6 +80,23 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'restapi.wsgi.application'
 
+
+
+#This is for django-registration-redux  check https://django-registration-redux.readthedocs.io/en/latest/quickstart.html
+ACCOUNT_ACTIVATION_DAYS = 7 # One-week activation window; you may, of course, use a different value.
+REGISTRATION_AUTO_LOGIN = True # Automatically log the user in.
+ACCOUNT_AUTHENTICATED_REGISTRATION_REDIRECTS=False
+SITE_ID=1  #activa la aplicacion para usar sesion
+LOGIN_REDIRECT_URL= 'profile'
+#REGISTRATION_ADMINS= [[('name1', 'name1@mail.com'), ('name2', 'name2@mail.com')]]
+
+
+#para enviar mail habilitar  https://accounts.google.com/DisplayUnlockCaptcha
+EMAIL_HOST='smtp.gmail.com' #for gmail
+#EMAIL_HOST_USER='example@gmail.com'
+#EMAIL_HOST_PASSWORD='paswd'
+EMAIL_PORT= 587# for gmail
+EMAIL_USE_TLS=True
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
